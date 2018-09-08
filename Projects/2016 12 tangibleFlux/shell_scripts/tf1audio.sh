05ddf1442380b61412e96243e5e62ea53af3d4a7
Navid <navid.nav@gmail.com>
2018-09-08 05:21:32 -0400

    Tweaking shell scripts

--- Projects/2016 12 tangibleFlux/shell_scripts/tf1audio.sh
+++ Projects/2016 12 tangibleFlux/shell_scripts/tf1audio.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 if [ $(ps aux | grep -v grep | grep "Max2.app" | wc -l) -eq 0 ] ;
 then
 		/Library/Ruby/Gems/2.3.0/gems/t-3.1.0/bin/t update "#1 audio down. Relaunching... ($date)"
 		/usr/bin/open -ga /Applications/Max2.app/Contents/MacOS/Max "/Users/tangibleflux/code/Nav/Projects/2016 12 tangibleFlux/TF-evan/tf_audio.maxpat" #reopen the audio patcher
+fi
--- Projects/2016 12 tangibleFlux/shell_scripts/tf3dmx.sh
+++ Projects/2016 12 tangibleFlux/shell_scripts/tf3dmx.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 if [ $(ps aux | grep -v grep | grep "Max.app" | wc -l) -eq 0 ] ;
 then
 		/Library/Ruby/Gems/2.3.0/gems/t-3.1.0/bin/t update "#3 DMX down. Relaunching... $(date)"
 		/usr/bin/open -ga "/Users/tangibleflux/code/Nav/Projects/2016 12 tangibleFlux/TF-cosm3/main_03.maxpat" #reopen the DMX patch
+fi
